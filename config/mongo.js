93de7b0bb125165605b0af53f1a37b01a6e90421
christonrinaldy <christonrinaldy.geodesy@gmail.com>
2022-07-28 15:28:21 +0700

    db uri example

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb://127.0.0.1:27017/"+process.env.DB_NAME;
+const uri =  process.env.DB_URI||"mongodb://127.0.0.1:27017/"+process.env.DB_NAME;
 
 const dbName = process.env.DB_NAME
 const client = new MongoClient(uri,{useUnifiedTopology:true});
